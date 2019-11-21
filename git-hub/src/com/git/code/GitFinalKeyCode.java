6303478c8dab0b869510d1ab2504ee8380d782a9
T K D Kameswara Rao <T K D Kameswara Rao@LAPTOP-6NA05F63>
2019-11-21 23:57:17 +0530

    1

---
+++ git-hub/src/com/git/code/GitFinalKeyCode.java
@@ -0,0 +1,11 @@
+package com.git.code;
+
+public class GitFinalKeyCode {
+	static final Integer id = 1;
+
+	public static void main(String[] args) {
+		final Integer idf = 5;
+		System.out.println(id);
+		System.out.println(idf);
+	}
+}
---
+++ git-hub/src/com/git/code/GitStreamCode.java
@@ -0,0 +1,10 @@
+package com.git.code;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GitStreamCode {
+	public static void main(String[] args) {
+		List<Integer> intgr = new ArrayList<Integer>();
+	}
+}
