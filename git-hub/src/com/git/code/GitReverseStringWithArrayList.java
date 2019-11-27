30d8e7dfe65043586afd37876127918c35c3cfaa
T K D Kameswara Rao <T K D Kameswara Rao@LAPTOP-6NA05F63>
2019-11-28 00:10:26 +0530

    String programs and concept programs

---
+++ git-hub/src/com/git/code/GitReverseStringWithArrayList.java
@@ -0,0 +1,24 @@
+package com.git.code;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.ListIterator;
+
+public class GitReverseStringWithArrayList {
+
+	public static void main(String[] args) {
+		String input = "Geeks For Geeks";
+		char[] hello = input.toCharArray();
+		List<Character> trial1 = new ArrayList<>();
+
+		for (char c : hello)
+			trial1.add(c);
+
+		Collections.reverse(trial1);
+		ListIterator li = trial1.listIterator();
+		while (li.hasNext())
+			System.out.print(li.next());
+	}
+
+}
---
+++ git-hub/src/com/git/code/GitReverseStringWithCharArray.java
@@ -0,0 +1,16 @@
+package com.git.code;
+
+public class GitReverseStringWithCharArray {
+
+	public static void main(String[] args) {
+		String input = "Geeks For Geeks";
+
+		// convert String to character array
+		// by using toCharArray
+		char[] try1 = input.toCharArray();
+
+		for (int i = try1.length - 1; i >= 0; i--)
+			System.out.print(try1[i]);
+	}
+
+}
